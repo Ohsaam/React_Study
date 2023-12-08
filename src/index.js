64d77a9bdde7bfa74c41bdebce19f953294e79b5
Ohsaam <ohjihwan123@naver.com>
2023-12-08 15:25:56 +0900

    page 추가 1. HomePagejsx 추가 2. NoticePage.jsx 추가 3. myReact에 import 추가함

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import React, { Fragment } from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import myReact from "./myReact";
+import MyReact from "./myReact";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "@fortawesome/fontawesome-free/js/all.js";
+import { BrowserRouter } from "react-router-dom";
 // document.querySelector("#root")
 const root = ReactDOM.createRoot(document.getElementById("root"));
+const a = 2;
 root.render(
   <>
-    <myReact />
+    <BrowserRouter>
+      <MyReact a={a} />
+    </BrowserRouter>
     {/* myReact 화살표함수 실행한다. 좌중괄호 우중괄호 안은 자바스크립트
      함수의 이름으로 태그를 선언한다. () 묶는다.->
      {}가 있으면 자바스크립트 영역이다.
--- src/myReact.jsx
+++ src/myReact.jsx
@@ -1,19 +1,33 @@
 import React, { Fragment } from "react";
+import { Route, Router } from "react-router-dom";
+import HomePage from "./components/page/HomePage";
+import NoticePage from "./components/page/NoticePage";
 //함수 - 객체 - 일급함수 - 고차함수
 /* 화살표 함수의 파라미터 자리는 props의 자리이다. 
    이 의미는 객체를 넘길 수 있다.
 */
-const myReact = (props) => {
+const MyReact = (props) => {
   //변수선언부
   //리턴 아래의 코드가 화면을 그린다(렌더링)
   //return 뒤에는 그려질 태그를 작성하는데 태그가 여러개면 괄호로 묶는다.
+  console.log({ props });
+  console.log(props.a);
   return (
     <>
-      <h2>myReact시스템</h2>
+      <div>
+        <Router>
+          <Route path="/" exact={true} element={<HomePage />} />
+          <Route
+            path="/notice/noticeList"
+            exact={true}
+            element={<NoticePage />}
+          />
+        </Router>
+      </div>
     </>
   );
 };
 
-export default myReact;
+export default MyReact;
 
 /* rafce 화살표함수로 Syntax제공 */
